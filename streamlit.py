779e627ab52492f20cea714dd4c26c0320a8e56a
PatrickCoop12 <123266129+PatrickCoop12@users.noreply.github.com>
2024-01-09 01:50:11 -0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -8,9 +8,8 @@ st.sidebar.markdown("Paste in the URL for the video you wish to extract audio fr
 x = st.sidebar.text_input('URL')
 if x is not None:
     yt = YouTube(x)
-    #stream = yt.streams.filter(only_audio=True)[0]
-    #stream.download()
-    st.sidebar.button('download',yt.streams.get_audio_only().download())
+    stream = yt.streams.filter(only_audio=True)[0]
+    st.sidebar.button('download',stream.download())
 
 
 
